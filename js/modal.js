4b52470fbb9f8fefe0e89c5488e95c7d14a578e8
Nazar8995 <nazar252003@gmail.com>
2022-08-23 00:43:31 +0300

    я хочу вскрыть себе вены

--- js/modal.js
+++ js/modal.js
@@ -20,3 +20,16 @@
     e.currentTarget.reset();
   });
 })();
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const mobileBtnClose = document.querySelector('[data-menu-close]');
+
+  menuBtnRef.addEventListener('click', () => {
+    mobileMenuRef.classList.toggle('is-open');
+  });
+
+  mobileBtnClose.addEventListener('click', () => {
+    mobileMenuRef.classList.toggle('is-open');
+  });
+})();
